dd15c3c2211ff8c411a9b9b9b81f50f958e7e2aa
Felix Ganzer <felixganzer@web.de>
2022-05-30 09:20:54 +0200

    test commit

--- src/mqtt_iobroker.h
+++ src/mqtt_iobroker.h
@@ -5,6 +5,7 @@
 #include <functional>
 #include "settings.h"
 #include "domain.h"
+#include <MQTT.h>
 
 namespace ArduMower
 {
@@ -20,13 +21,18 @@ namespace ArduMower
         ArduMower::Domain::Robot::CommandExecutor &cmd;
         std::function<bool(const String &, const String &)> tx;
 
+        MQTTClient &client;
+
       public:
         Adapter(ArduMower::Modem::Settings::Settings &_settings,
                 ArduMower::Domain::Robot::StateSource &_source,
                 ArduMower::Domain::Robot::CommandExecutor &_cmd,
                 std::function<bool(const String &, const String &)> _tx)
-            : settings(_settings), source(_source), cmd(_cmd), tx(_tx){};      
+            : settings(_settings), source(_source), cmd(_cmd), tx(_tx){};  
+
+        setMQTTClient(MQTTClient &_client):client(_client){};    
       };
+
     }
   }
 }
