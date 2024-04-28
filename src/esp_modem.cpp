aaad10dccdff5ccf56de791c689bb08375412f4d
Tim Otto <tim@timotto.io>
2024-04-28 16:07:17 +0200

    🙋 explicitly configure Serial2 pins

--- src/esp_modem.cpp
+++ src/esp_modem.cpp
@@ -79,7 +79,7 @@ Prometheus::LooptimeMonitor looptime;
 
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, 16, 17);
 
   api.begin(&bleAdapter, &relayAdapter);
   settings.begin();
