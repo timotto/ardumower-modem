e487cc1d85084289a2157b12730545fba0608ab7
Tim Otto <tim@timotto.io>
2024-04-28 15:04:12 +0200

    🦾 build test tools for correct runner cpu arch

--- ci/bin/build-test-tools.sh
+++ ci/bin/build-test-tools.sh
@@ -5,9 +5,9 @@ set -e
 artifacts="$PWD/test-artifacts"
 
 cd tests/util/serial_test_runner
-go build -o "$artifacts/serial_test_runner" .
+GOOS=linux GOARCH=arm64 go build -o "$artifacts/serial_test_runner" .
 cd -
 
 cd tests/util/validation
-go build -o "$artifacts/validate" ./cmd/validate
+GOOS=linux GOARCH=arm64 go build -o "$artifacts/validate" ./cmd/validate
 cd -
